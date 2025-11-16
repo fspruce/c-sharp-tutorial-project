8c4bf3c1bfd8410fbc16c5d3b27d53e51f58f1eb
fspruce <f.spruce@proton.me>
2025-11-16 15:42:37 +0000

    Complete Times Table Exercise

---
+++ Exercise_Times_Tables/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_Times_Tables
+{
+    internal class Program
+    {
+        /*
+         * Ask the user for a number for the table.
+         * Write a for loop to print X times table.
+         */
+        static void Main(string[] args)
+        {
+            int timesTable = 0;
+            bool success = false;
+
+            Console.WriteLine("Choose an integer and I will print out the corresponding times table!");
+            Console.WriteLine();
+
+            while (!success)
+            {
+                Console.Write("Please enter an integer: ");
+                success = int.TryParse(Console.ReadLine(), out timesTable );
+
+                if (success) 
+                {   
+                    Console.WriteLine();
+                    Console.WriteLine($"The {timesTable} Times Table!");
+                    Console.WriteLine("------------------------------");
+                    for (int i = 0; i <= 12; i++)
+                    {
+                        
+                        Console.WriteLine($"{i} x {timesTable} = {i * timesTable}");
+                    }
+                } else
+                {
+                    Console.WriteLine("Please enter a valid integer.");
+                    Console.WriteLine();
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
