38f27f6410f9a646f2107470f58b35a15c6d785b
fspruce <f.spruce@proton.me>
2025-11-16 15:00:17 +0000

    Complete conditional operator

---
+++ ConditionalOperator/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConditionalOperator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int age = -10;
+
+            /*if (age >= 0)
+            {
+                Console.WriteLine("Valid");
+            } else
+            {
+                Console.WriteLine("Invalid");
+            }*/
+
+            //condition ? true : false
+            Console.WriteLine(age >= 10 ? "Valid" : "Invalid");
+
+            Console.ReadLine();
+        }
+    }
+}
