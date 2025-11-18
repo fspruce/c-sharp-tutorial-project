f21e52a0921ecbe28596b033b71614d2f5d73923
fspruce <f.spruce@proton.me>
2025-11-18 13:22:41 +0000

    Complete IsNullOrEmpty

---
+++ IsNullOrEmpty/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IsNullOrEmpty
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine();
+
+            Console.WriteLine($"Your name is {name}.");
+
+            if (name != "")
+                Console.WriteLine("0");
+
+            if (!name.Equals(""))
+                Console.WriteLine("1");
+
+            if (!string.IsNullOrEmpty(name)) //Prevents null reference exceptions if the value is set to null.
+                Console.WriteLine("2");
+
+            Console.ReadLine();
+        }
+    }
+}
